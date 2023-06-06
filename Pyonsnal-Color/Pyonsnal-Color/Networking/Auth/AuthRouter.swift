8b567ed51d9ea85d4db13b949b31c7de0aff2fa1
kimjinu <kimjinu5555@gmail.com>
2023-06-06 17:13:06 +0900

    Add: AuthRouter 구현체 추가

---
+++ Pyonsnal-Color/Pyonsnal-Color/Networking/Auth/AuthRouter.swift
@@ -0,0 +1,85 @@
+//
+//  AuthRouter.swift
+//  Pyonsnal-Color
+//
+//  Created by 김진우 on 2023/06/06.
+//
+
+import Foundation
+import Alamofire
+
+enum AuthRouter: URLRequestConvertible {
+    case apple(token: String)
+
+    private var baseURLString: String {
+        #if DEBUG
+        return ""
+        #else
+        return ""
+        #endif
+    }
+
+    var method: HTTPMethod {
+        switch self {
+        case .apple:
+            return .post
+        }
+    }
+
+    var path: String {
+        switch self {
+        case .apple:
+            return "/auth/apple"
+        }
+    }
+
+    var bodyData: Data? {
+        switch self {
+        case let .apple(token):
+            let body: [String: String] = ["token": token]
+            let data = try? JSONEncoder().encode(body)
+            return data
+        }
+    }
+
+    var queryString: [URLQueryItem] {
+        switch self {
+        case .apple:
+            return []
+        }
+    }
+
+    var header: [String: String] {
+        switch self {
+        case .apple:
+            return [:]
+        }
+    }
+
+    func asURLRequest() throws -> URLRequest {
+        // Set URL
+        var urlComponent: URLComponents? = .init(string: baseURLString + path)
+        if !queryString.isEmpty {
+            urlComponent?.queryItems = queryString
+        }
+        guard let url: URL = urlComponent?.url else {
+            fatalError("asURLRequest() - URL get fail")
+        }
+        var request: URLRequest = .init(url: url)
+
+        // Set Method
+        request.method = method
+
+        // Set Header
+        header.forEach { (key, value) in
+            request.setValue(value, forHTTPHeaderField: key)
+        }
+
+        // Set Body
+        if let bodyData {
+            request.httpBody = bodyData
+        }
+
+        return request
+    }
+}
