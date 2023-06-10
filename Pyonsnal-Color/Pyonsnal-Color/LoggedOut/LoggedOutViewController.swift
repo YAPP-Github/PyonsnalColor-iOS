00176b01f0cc5f499ba4792516801845db7e8623
kimjinu <kimjinu5555@gmail.com>
2023-06-10 19:32:05 +0900

    Update: LoggedOutViewController UI 코드 수정 (ViewHolder 사용하도록)

--- Pyonsnal-Color/Pyonsnal-Color/LoggedOut/LoggedOutViewController.swift
+++ Pyonsnal-Color/Pyonsnal-Color/LoggedOut/LoggedOutViewController.swift
@@ -22,80 +22,45 @@ final class LoggedOutViewController:
 
     // MARK: - Interface
     weak var listener: LoggedOutPresentableListener?
-
-    // MARK: - UI Component
-    private let loginButtonStackView: UIStackView = {
-        let stackView: UIStackView = .init()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .vertical
-        stackView.spacing = 8
-        stackView.distribution = .fill
-        return stackView
-    }()
-    
-    private let appleLoginButton = ASAuthorizationAppleIDButton()
-
-    private let kakaoLoginButton: UIButton = {
-        let button: UIButton = .init()
-        button.layer.cornerRadius = 8
-        button.backgroundColor = .systemBrown
-        button.setTitle("🍈 Kakao Login", for: .normal)
-        button.setTitleColor(.systemYellow, for: .normal)
-        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .semibold)
-        return button
-    }()
+    private let viewHolder: ViewHolder = .init()
 
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        viewHolder.place(in: view)
+        viewHolder.configureConstraints(for: view)
+
         configureUI()
-        configureLayout()
         configureAction()
     }
 
     // MARK: - Private Method
     private func configureUI() {
-        view.backgroundColor = .green
+        view.backgroundColor = .white
     }
-    
-    private func configureLayout() {
-        view.addSubview(loginButtonStackView)
 
-        loginButtonStackView.addArrangedSubview(appleLoginButton)
-        loginButtonStackView.addArrangedSubview(kakaoLoginButton)
-
-        NSLayoutConstraint.activate([
-            loginButtonStackView.widthAnchor.constraint(equalToConstant: 220),
-            loginButtonStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            loginButtonStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-    }
-    
     private func configureAction() {
-        appleLoginButton.addTarget(
+        viewHolder.appleLoginButton.addTarget(
             self,
             action: #selector(didTapAppleLoginButton),
             for: .touchUpInside)
-        
-        kakaoLoginButton.addTarget(
+
+        viewHolder.kakaoLoginButton.addTarget(
             self,
             action: #selector(didTapKakaoLoginButton),
             for: .touchUpInside
         )
     }
-    
+
     @objc
     private func didTapAppleLoginButton() {
         listener?.didTapAppleLoginButton()
     }
-    
+
     @objc
     private func didTapKakaoLoginButton() {
         listener?.requestKakaoLogin()
     }
 
 }
-
-
-
