275f47d71a0b9fe2b22c3e08d9a691a1aa99a7fc
lnhk98 <hozzang666@gmail.com>
2023-06-10 17:46:45 +0900

    Feat: ProfileHome 리블렛 추가

---
+++ Pyonsnal-Color/Pyonsnal-Color/ProfileHome/ProfileHomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileHomeViewController.swift
+//  Pyonsnal-Color
+//
+//  Created by 김인호 on 2023/06/10.
+//
+
+import ModernRIBs
+import UIKit
+
+protocol ProfileHomePresentableListener: AnyObject {
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
+}
+
+final class ProfileHomeViewController: UIViewController, ProfileHomePresentable, ProfileHomeViewControllable {
+
+    weak var listener: ProfileHomePresentableListener?
+}
