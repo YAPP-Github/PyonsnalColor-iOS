e845772521544bf8ae0c52458a176f4f2a037fda
kimjinu <kimjinu5555@gmail.com>
2023-06-26 21:57:27 +0900

    Add: CGFloat에 Spacing 인터페이스 추가

---
+++ Pyonsnal-Color/Pyonsnal-Color/Extension/CGFloat+.swift
@@ -0,0 +1,20 @@
+//
+//  CGFloat+.swift
+//  Pyonsnal-Color
+//
+//  Created by 김진우 on 2023/06/24.
+//
+
+import Foundation
+
+extension CGFloat {
+    var spacing2: CGFloat { Spacing.spacing2.value }
+    var spacing4: CGFloat { Spacing.spacing4.value }
+    var spacing8: CGFloat { Spacing.spacing8.value }
+    var spacing12: CGFloat { Spacing.spacing12.value }
+    var spacing16: CGFloat { Spacing.spacing16.value }
+    var spacing20: CGFloat { Spacing.spacing20.value }
+    var spacing24: CGFloat { Spacing.spacing24.value }
+    var spacing32: CGFloat { Spacing.spacing32.value }
+    var spacing40: CGFloat { Spacing.spacing40.value }
+}
