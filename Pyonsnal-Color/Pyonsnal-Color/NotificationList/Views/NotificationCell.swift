35e428fb32e98d04a2d10009c2deda255e99cf2a
lnhk98 <hozzang666@gmail.com>
2023-06-30 15:47:59 +0900

    Feat: 알림함 리스트에 사용되는 NotificationCell 클래스 생성 및 구현

---
+++ Pyonsnal-Color/Pyonsnal-Color/NotificationList/Views/NotificationCell.swift
@@ -0,0 +1,136 @@
+//
+//  NotificationCell.swift
+//  Pyonsnal-Color
+//
+//  Created by 김인호 on 2023/06/28.
+//
+
+import UIKit
+import SnapKit
+
+final class NotificationCell: UITableViewCell {
+    static let identifier: String = .init(describing: NotificationCell.self)
+    
+    let viewHolder: ViewHolder = .init()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        viewHolder.place(in: contentView)
+        viewHolder.configureConstraints(for: contentView)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureCell(with notification: Notification) {
+        viewHolder.descriptionLabel.text = notification.description
+        viewHolder.dateLabel.text = notification.date
+        contentView.backgroundColor = .clear
+        selectionStyle = .none
+        viewHolder.containerStackView.makeRounded(with: 16)
+    }
+}
+
+extension NotificationCell {
+    enum Size {
+        static let containerMargin: UIEdgeInsets = .init(top: 16, left: 16, bottom: 16, right: 16)
+        static let containerSpacing: CGFloat = 20
+        
+        static let innerStackViewSpacing: CGFloat = 4
+    }
+    
+    final class ViewHolder: ViewHolderable {
+        
+        let containerStackView: UIStackView = {
+            let stackView = UIStackView()
+            stackView.axis = .horizontal
+            stackView.isLayoutMarginsRelativeArrangement = true
+            stackView.layoutMargins = Size.containerMargin
+            stackView.alignment = .top
+            stackView.spacing = Size.containerSpacing
+            stackView.backgroundColor = .white
+            stackView.makeRounded(with: 16)
+            return stackView
+        }()
+        
+        let categoryImageView: UIImageView = {
+            let imageView = UIImageView()
+            imageView.backgroundColor = .blue
+            imageView.makeRounded(with: 18)
+            return imageView
+        }()
+        
+        private let innerStackView: UIStackView = {
+            let stackView = UIStackView()
+            stackView.axis = .vertical
+            stackView.alignment = .leading
+            stackView.spacing = Size.innerStackViewSpacing
+            return stackView
+        }()
+        
+        let categoryTagImageView: UIImageView = {
+            let imageView = UIImageView()
+            imageView.backgroundColor = .systemPink
+            imageView.makeRounded(with: 20)
+            return imageView
+        }()
+        
+        let descriptionLabel: UILabel = {
+            let label = UILabel()
+            label.font = .body2r
+            label.numberOfLines = 0
+            return label
+        }()
+        
+        let dateLabel: UILabel = {
+            let label = UILabel()
+            label.font = .body3r
+            //TODO: color 적용
+            label.textColor = .systemGray
+            return label
+        }()
+        
+        let spacingView: UIView = {
+            let view = UIView()
+            view.backgroundColor = .clear
+            return view
+        }()
+        
+        func place(in view: UIView) {
+            view.addSubview(containerStackView)
+            view.addSubview(spacingView)
+            
+            containerStackView.addArrangedSubview(categoryImageView)
+            containerStackView.addArrangedSubview(innerStackView)
+            
+            innerStackView.addArrangedSubview(categoryTagImageView)
+            innerStackView.addArrangedSubview(descriptionLabel)
+            innerStackView.addArrangedSubview(dateLabel)
+        }
+        
+        func configureConstraints(for view: UIView) {
+            containerStackView.snp.makeConstraints { make in
+                make.top.leading.trailing.equalTo(view)
+            }
+            
+            categoryImageView.snp.makeConstraints { make in
+                make.width.equalTo(36)
+                make.height.equalTo(36)
+            }
+            
+            categoryTagImageView.snp.makeConstraints { make in
+                make.width.equalTo(73)
+                make.height.equalTo(26)
+            }
+            
+            spacingView.snp.makeConstraints { make in
+                make.height.equalTo(12  )
+                make.top.equalTo(containerStackView.snp.bottom)
+                make.leading.trailing.equalTo(containerStackView)
+                make.bottom.equalToSuperview()
+            }
+        }
+    }
+}
