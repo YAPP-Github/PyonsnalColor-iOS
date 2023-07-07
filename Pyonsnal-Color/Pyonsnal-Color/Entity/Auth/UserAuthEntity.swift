a69530ceb3db154062ad193cb7fc914a41eb87a9
kimjinu <kimjinu5555@gmail.com>
2023-07-08 04:32:47 +0900

    Add: UserAuthEntity 추가

---
+++ Pyonsnal-Color/Pyonsnal-Color/Entity/Auth/UserAuthEntity.swift
@@ -0,0 +1,12 @@
+//
+//  UserAuthEntity.swift
+//  Pyonsnal-Color
+//
+//  Created by 김진우 on 2023/07/08.
+//
+
+struct UserAuthEntity: Codable {
+    let isFirstLogin: Bool
+    let accessToken: String
+    let refreshToken: String
+}
