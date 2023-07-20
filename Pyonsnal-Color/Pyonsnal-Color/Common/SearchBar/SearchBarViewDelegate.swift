d75d32d1b44774171f949c64049292435661707c
kimjinu <kimjinu5555@gmail.com>
2023-07-21 02:33:16 +0900

    Add: SearchBarView 구현

---
+++ Pyonsnal-Color/Pyonsnal-Color/Common/SearchBar/SearchBarViewDelegate.swift
@@ -0,0 +1,10 @@
+//
+//  SearchBarViewDelegate.swift
+//  Pyonsnal-Color
+//
+//  Created by 김진우 on 2023/07/21.
+//
+
+protocol SearchBarViewDelegate: AnyObject {
+    func updateText(_ text: String?)
+}
