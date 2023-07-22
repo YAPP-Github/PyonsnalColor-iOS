31ee143bce394637a1e6fe40769e92a5ff43f70a
Jouureee <Jouureee@gmail.com>
2023-07-22 23:46:36 +0900

    Add: 필요한 이미지 asset 파일 추가

---
+++ Pyonsnal-Color/Pyonsnal-Color/Resource/ImageAssetKind/ImageAsset+Filter.swift
@@ -0,0 +1,19 @@
+//
+//  ImageAsset+Filter.swift
+//  Pyonsnal-Color
+//
+//  Created by 조소정 on 2023/07/22.
+//
+
+import UIKit
+
+extension ImageAssetKind {
+    enum Filter: String {
+        case sortFilter = "icon_filter_view"
+        case filter = "icon_filter"
+        
+        var image: UIImage? {
+            return .init(named: self.rawValue)
+        }
+    }
+}
