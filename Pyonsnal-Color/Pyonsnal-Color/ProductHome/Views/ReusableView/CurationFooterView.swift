8f772bae1dc2b95102b4b3b7303cd15366ac01f7
lnhk98 <hozzang666@gmail.com>
2023-07-24 20:34:12 +0900

    Feat: CurationFooterView 클래스 생성

---
+++ Pyonsnal-Color/Pyonsnal-Color/ProductHome/Views/ReusableView/CurationFooterView.swift
@@ -0,0 +1,36 @@
+//
+//  CurationFooterView.swift
+//  Pyonsnal-Color
+//
+//  Created by 김인호 on 2023/07/24.
+//
+
+import UIKit
+import SnapKit
+
+final class CurationFooterView: UICollectionReusableView {
+    private let dividerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray100
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureLayout() {
+        addSubview(dividerView)
+        
+        dividerView.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.height.equalTo(12)
+        }
+    }
+}
