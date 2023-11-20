020faeb4e4547e4e58e5ac99054f2320df951e1c
Jouureee <spqjf12345@gmail.com>
2023-11-21 01:10:28 +0900

    Add: AlertActionSheet 연결 및 카메라 연결 권한 허용

--- Pyonsnal-Color/Pyonsnal-Color/ProfileEdit/ProfileEditViewController.swift
+++ Pyonsnal-Color/Pyonsnal-Color/ProfileEdit/ProfileEditViewController.swift
@@ -8,11 +8,19 @@
 import UIKit
 import Combine
 import ModernRIBs
+import PhotosUI
 
 protocol ProfileEditPresentableListener: AnyObject {
     func didTapEditButton()
     func didTapBackButton()
     func editNickname(nickname: String)
+}
+
+enum EditActionSheetSection: String {
+    case camera = "카메라로 촬영하기"
+    case gallery = "갤러리에서 선택하기"
+    case delete = "프로필 사진 삭제"
+    case cancel = "취소"
 }
 
 final class ProfileEditViewController: UIViewController, ProfileEditPresentable, ProfileEditViewControllable {
@@ -21,6 +29,7 @@ final class ProfileEditViewController: UIViewController, ProfileEditPresentable,
     private var viewHolder: ViewHolder = .init()
     private var cancellable = Set<AnyCancellable>()
     private var maximumNicknameCount: Int = 15
+    let imageUploadSize: CGSize = .init(width: 1080, height: 1080)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -62,9 +71,7 @@ final class ProfileEditViewController: UIViewController, ProfileEditPresentable,
         viewHolder.imageContainerView
             .gesturePublisher()
             .sink { [weak self] _ in
-                // TODO: alert 띄우기
-                let alertController = UIAlertController()
-                
+                self?.showActionSheet()
             }.store(in: &cancellable)
         
         viewHolder.nicknameTextField
@@ -101,6 +108,47 @@ final class ProfileEditViewController: UIViewController, ProfileEditPresentable,
         let count = viewHolder.nicknameTextField.text?.count ?? 0
         viewHolder.nicknameCountLabel.text = "\(count)/\(Constant.maximumNicknameCount)"
     }
+    
+    private func showActionSheet() {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let cameraAction = UIAlertAction(title: EditActionSheetSection.camera.rawValue, style: .default) { [weak self] _ in
+            self?.showImagePicker()
+        }
+        let galleryAction = UIAlertAction(title: EditActionSheetSection.gallery.rawValue, style: .default) { [weak self] _ in
+            self?.showPHPicker()
+        }
+        let deleteAction = UIAlertAction(title: EditActionSheetSection.delete.rawValue, style: .destructive) { [weak self] _ in
+            self?.deleteProfileImage()
+        }
+        let cancelAction = UIAlertAction(title: EditActionSheetSection.cancel.rawValue, style: .cancel)
+        
+        alertController.addAction(cameraAction)
+        alertController.addAction(galleryAction)
+        alertController.addAction(deleteAction)
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true)
+    }
+    
+    private func showImagePicker() {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.allowsEditing = true
+        picker.delegate = self
+        self.present(picker, animated: true)
+    }
+    
+    private func showPHPicker() {
+        var configuration = PHPickerConfiguration()
+        configuration.selectionLimit = 1
+        configuration.filter = .images
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = self
+        self.present(picker, animated: true)
+    }
+    
+    private func deleteProfileImage() {
+        viewHolder.profileImageView.setImage(.tagStore)
+    }
 }
 
 // MARK: - BackNavigationViewDelegate
@@ -119,6 +167,39 @@ extension ProfileEditViewController: UITextFieldDelegate {
             return false
         }
         return true
+    }
+}
+
+// MARK: UIImagePickerControllerDelegate, UINavigationControllerDelegate
+extension ProfileEditViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            let resizedImage = editedImage.resize(targetSize: imageUploadSize)
+            viewHolder.profileImageView.image = resizedImage
+        }
+        
+    }
+}
+
+// MARK: - PHPickerViewControllerDelegate
+extension ProfileEditViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+        guard let itemProvider = results.map(\.itemProvider).first else {
+            return
+        }
+        if itemProvider.canLoadObject(ofClass: UIImage.self) {
+            itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
+                guard let self,
+                      let image = image as? UIImage,
+                      error == nil else { return }
+                let resizedImage = image.resize(targetSize: imageUploadSize)
+                DispatchQueue.main.async {
+                    self.viewHolder.profileImageView.image = resizedImage
+                }
+            }
+        }
     }
 }
 
@@ -151,7 +232,9 @@ extension ProfileEditViewController {
         
         let profileImageView: UIImageView = {
             let imageView = UIImageView()
-            imageView.contentMode = .scaleAspectFit
+            imageView.contentMode = .scaleAspectFill
+            imageView.makeRounded(with: 50)
+            imageView.makeBorder(width: 1, color: UIColor.gray300.cgColor)
             imageView.setImage(.tagStore)
             return imageView
         }()
